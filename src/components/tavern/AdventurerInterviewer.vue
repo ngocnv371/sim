b1b755e383445289b417aad1d4a8c1b77da98a62
Ngoc Ngo <ngocnv371@gmail.com>
2021-05-06 17:31:04 +0700

    flattern tavern

---
+++ src/components/tavern/AdventurerInterviewer.vue
@@ -0,0 +1,18 @@
+<template>
+  <AdventurerThumbnail :adventurer="adventurer"></AdventurerThumbnail>
+</template>
+
+<script>
+import AdventurerThumbnail from "@/components/character/AdventurerThumbnail.vue";
+export default {
+  components: {
+    AdventurerThumbnail,
+  },
+  props: {
+    adventurer: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- src/components/tavern/List.vue
+++ src/components/tavern/List.vue
@@ -1,32 +1,21 @@
 <template>
-  <v-item-group mandatory v-model="selectedItem">
+  <v-card flat>
     <v-container>
-      <v-row>
-        <v-col v-for="one of adventurers" :key="one.id" cols="4" md="3" class="pa-0 ma-auto">
-          <v-item v-slot="{ active, toggle }">
-            <AdventurerCard :adventurer="one" :active="active" @click="toggle" />
-          </v-item>
+      <v-row dense>
+        <v-col v-for="one of adventurers" :key="one.id" cols="3">
+          <AdventurerInterviewer :adventurer="one" />
         </v-col>
       </v-row>
     </v-container>
-  </v-item-group>
+  </v-card>
 </template>
 
 <script lang="ts">
-import AdventurerCard from "@/components/character/AdventurerCard.vue";
+import AdventurerInterviewer from "@/components/tavern/AdventurerInterviewer.vue";
 import { mapGetters } from "vuex";
-import { Character } from "@/core/character/Character";
 
-interface Data {
-  selectedItem: Character | null;
-}
 export default {
-  components: { AdventurerCard },
-  data(): Data {
-    return {
-      selectedItem: null,
-    };
-  },
+  components: { AdventurerInterviewer },
   computed: {
     ...mapGetters("tavern", ["adventurers"]),
   },
